99341022f1ee6b915111f6b2a165fa8d594de228
Dmitry-Lipinskiy <Dimalipa@mail.ru>
2022-08-23 23:14:28 +0300

    Init app

--- src/app/shared/service/film.service.ts
+++ src/app/shared/service/film.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IInfo, ISearch } from '../model/film-app.model';
 
-const URL_API = 'http://www.omdbapi.com';
+const URL_API = 'https://www.omdbapi.com';
 const API_KEY = 'ce47be69';
 
 @Injectable({
